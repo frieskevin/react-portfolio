f9e3935270f3c94544bf38b25ed9edcf27c156fb
alotofsnakes <mrfries@gmail.com>
2022-11-29 18:26:36 -0800

    made container page

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -2,5 +2,23 @@ import React, { useState } from 'react';
 
 export default function PortfolioContainer() {
     const [currentPage, setCurrentPage] = useState('Home');
-    const renderPage = (
+    const renderPage = () => {
+        if (currentPage === 'Portfolio') {
+            return <Portfolio />
+        }
+        if (currentPage === 'Contact') {
+            return <Contact />
+        }
+        if (currentPage === 'Resume') {
+            return <Resume />
+        }
+        return <About />
+    };
+    const handlePageChange = (page) => setCurrentPage(page);
+    return (
+        <div>
+            <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+            {renderPage()}
+        </div>
+    );
 }
